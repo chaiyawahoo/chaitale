2def976fe18caf17a580367e495e349774c6919e
Chai Nunes <chaiyawahoo@gmail.com>
2024-04-19 18:37:15 -0700

    fps and vsync settings

--- game/settings.gd
+++ game/settings.gd
@@ -11,6 +11,12 @@ var voxel_interaction_reach: float = 6.0
 
 var window_mode: Window.Mode = Window.MODE_WINDOWED
 
+var cap_fps: bool = false
+var max_fps: int = 60
+var fps_minimum: int = 30
+var fps_maximum: int = 500
+var vsync_mode: DisplayServer.VSyncMode = DisplayServer.VSYNC_ENABLED
+
 var fov: float = 75
 var fov_minimum: float = 30
 var fov_maximum: float = 110
@@ -36,6 +42,9 @@ func load_settings() -> void:
 	
 	window_mode = config.get_value("video", "window_mode", window_mode)
 	fov = config.get_value("video", "fov", fov)
+	cap_fps = config.get_value("video", "cap_fps", cap_fps)
+	max_fps = config.get_value("video", "max_fps", max_fps)
+	vsync_mode = config.get_value("video", "vsync_mode", vsync_mode)
 	mouse_sensitivity = config.get_value("controls", "mouse_sensitivity", mouse_sensitivity)
 
 	update_settings()
@@ -45,9 +54,17 @@ func update_settings() -> void:
 	var config: ConfigFile = ConfigFile.new()
 	
 	get_viewport().get_window().mode = window_mode
+	DisplayServer.window_set_vsync_mode(vsync_mode)
+	Engine.max_fps = max_fps
+	if not cap_fps:
+		Engine.max_fps = 0
+		
 
 	config.set_value("video", "window_mode", window_mode)
 	config.set_value("video", "fov", fov)
+	config.set_value("video", "cap_fps", cap_fps)
+	config.set_value("video", "max_fps", max_fps)
+	config.set_value("video", "vsync_mode", vsync_mode)
 	config.set_value("controls", "mouse_sensitivity", mouse_sensitivity)
 
 	config.save("user://settings.cfg")
--- ui/settings_menu.gd
+++ ui/settings_menu.gd
@@ -13,6 +13,10 @@ func _enter_tree() -> void:
 	%SensitivityInput.text_submitted.connect(_on_sensitivity_text_submitted)
 	%FovSlider.value_changed.connect(_on_fov_slider_changed)
 	%FovInput.text_submitted.connect(_on_fov_text_submitted)
+	%CapFpsCheck.toggled.connect(_on_cap_fps_toggled)
+	%FpsSlider.value_changed.connect(_on_max_fps_slider_changed)
+	%FpsInput.text_submitted.connect(_on_max_fps_text_submitted)
+	%VsyncCheck.toggled.connect(_on_vsync_toggled)
 
 
 func _ready() -> void:
@@ -26,6 +30,13 @@ func _ready() -> void:
 	%SensitivityInput.text = "%.3f" % Settings.mouse_sensitivity
 	%FovSlider.value = Settings.fov
 	%FovInput.text = "%d" % roundi(Settings.fov)
+	%CapFpsCheck.button_pressed = Settings.cap_fps
+	%FpsInput.editable = Settings.cap_fps
+	%FpsSlider.editable = Settings.cap_fps
+	%FramerateLabel.theme_type_variation = "" if Settings.cap_fps else "DisabledLabel"
+	%FpsSlider.value = Settings.max_fps
+	%FpsInput.text = "%d" % Settings.max_fps
+	%VsyncCheck.button_pressed = false if Settings.vsync_mode == DisplayServer.VSYNC_DISABLED else true
 
 
 func _input(event: InputEvent) -> void:
@@ -34,8 +45,9 @@ func _input(event: InputEvent) -> void:
 		get_viewport().set_input_as_handled()
 
 
-func _on_window_mode_selected(_index: int):
+func _on_window_mode_selected(_index: int) -> void:
 	update_window_mode()
+
 
 func _on_sensitivity_text_submitted(new_text: String) -> void:
 	update_sensitivity(new_text)
@@ -53,6 +65,22 @@ func _on_fov_slider_changed(value: float) -> void:
 	update_fov(str(value))
 
 
+func _on_cap_fps_toggled(button_pressed: bool) -> void:
+	update_fps_cap(button_pressed)
+
+
+func _on_max_fps_text_submitted(new_text: String) -> void:
+	update_max_fps(new_text)
+
+
+func _on_max_fps_slider_changed(value: float) -> void:
+	update_max_fps(str(value))
+
+
+func _on_vsync_toggled(button_pressed: bool) -> void:
+	update_vsync(button_pressed)
+
+
 func _on_button_back() -> void:
 	back()
 
@@ -66,33 +94,48 @@ func _on_button_apply_close() -> void:
 	back()
 
 
-func update_window_mode():
+func update_window_mode() -> void:
 	var selected = %WindowMode.get_selected_id()
-	if selected == Settings.window_mode:
-		return
+
 	unapplied_settings.window_mode = selected
 
 
-func update_sensitivity(value: String):
+func update_sensitivity(value: String) -> void:
 	var corrected_text: String = "%.3f" % clampf(float(value), 0, 10)
 	var corrected_float: float = float(corrected_text)
 	%SensitivitySlider.value = corrected_float
 	%SensitivityInput.text = corrected_text
 
-	if corrected_float == Settings.mouse_sensitivity:
-		return	
 	unapplied_settings.mouse_sensitivity = corrected_float
 
 
-func update_fov(value: String):
+func update_fov(value: String) -> void:
 	var corrected_text: String = "%d" % roundi(clampf(float(value), Settings.fov_minimum, Settings.fov_maximum))
 	var corrected_float: float = float(corrected_text)
 	%FovSlider.value = corrected_float
 	%FovInput.text = corrected_text
 
-	if corrected_float == Settings.fov:
-		return
 	unapplied_settings.fov = corrected_float
+
+
+func update_fps_cap(button_pressed: bool) -> void:
+	%FpsInput.editable = button_pressed
+	%FpsSlider.editable = button_pressed
+	%FramerateLabel.theme_type_variation = "" if button_pressed else "DisabledLabel"
+
+	unapplied_settings.cap_fps = button_pressed
+
+
+func update_max_fps(value: String) -> void:
+	var corrected_text: String = "%d" % clampi(int(value), Settings.fps_minimum, Settings.fps_maximum)
+	var corrected_int: float = float(corrected_text)
+	%FpsSlider.value = corrected_int
+	%FpsInput.text = corrected_text
+
+	unapplied_settings.max_fps = corrected_int
+
+func update_vsync(button_pressed: bool) -> void:
+	unapplied_settings.vsync_mode = DisplayServer.VSYNC_ENABLED if button_pressed else DisplayServer.VSYNC_DISABLED
 
 
 func back() -> void:
@@ -106,6 +149,9 @@ func apply_changes() -> void:
 	update_window_mode()
 	update_sensitivity(%SensitivityInput.text)
 	update_fov(%FovInput.text)
+	update_fps_cap(%CapFpsCheck.button_pressed)
+	update_max_fps(%FpsInput.text)
+	update_vsync(%VsyncCheck.button_pressed)
 	for setting_key in unapplied_settings:
 		Settings.set(setting_key, unapplied_settings[setting_key])
 	Settings.update_settings()
